7ff32672ec96487b30ff4f5faddbdbeaa228cad2
Animesh Pandey <apandey@traackr.com>
2016-06-26 21:33:47 -0400

    refactoring

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         process.env.OPENSHIFT_APP_NAME;
 }
 
+console.log(connectionString);
 mongoose.connect(connectionString);
 console.log("Connection status of mongodb: " + mongoose.connection.readyState);
 
