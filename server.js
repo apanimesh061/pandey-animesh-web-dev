00855ce6cb83aa815ec5abeccf8431746c570564
Animesh Pandey <apandey@traackr.com>
2016-06-26 21:40:12 -0400

    refactoring

--- server.js
+++ server.js
@@ -35,9 +35,7 @@ project(app);
 
 var connectionString = 'mongodb://127.0.0.1:27017/test';
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
 }
